6373b5144cda7c534a722e8ea34544fd76644346
Guoliang Li <guoliang.me@gmail.com>
2017-12-31 19:06:02 +0800

    play around with GUI

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import os
 from Tkinter import *
-
-import windows_ops
+from ttk import Combobox
 
 root = Tk()
-w = Label(root, text="hello world")
-w.pack()
 
-e = Entry(root)
-e.pack()
 
-b = Button(root, text="run")
-b.pack()
+def on_field_change(index, value, op):
+    print "combobox updated to: ", cb.get()
+    cb.bind("<Down>", lambda e: None)
+
+
+v = StringVar()
+v.trace('w', on_field_change)
+
+cb = Combobox(root, textvar=v, values=("one", "two", "three"))
+cb.pack()
+cb.focus()
 
 if os.name == 'nt':
+    import windows_ops
+
     windows_ops.register_hot_key()
     root.after(1, windows_ops.hotkey_handler, root)
 
