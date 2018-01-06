72459fbc0455b78987e2ab51b2de3c84d21582a6
Guoliang Li <guoliang.me@gmail.com>
2018-01-06 16:30:23 +0800

    play around with GUI - entry + listbox

--- app.py
+++ app.py
@@ -1,26 +1,70 @@
 import os
+import subprocess
 from Tkinter import *
-from ttk import Combobox
 
 root = Tk()
 
 
-def on_field_change(index, value, op):
-    print "combobox updated to: ", cb.get()
-    cb.bind("<Down>", lambda e: None)
+def get_select_options(input):
+    return [input + "-A", input + "-B", input + "-C"]
 
 
-v = StringVar()
-v.trace('w', on_field_change)
+def update_select_options(input):
+    listbox.delete(0, END)
+    for item in get_select_options(input):
+        listbox.insert(END, item)
 
-cb = Combobox(root, textvar=v, values=("one", "two", "three"))
-cb.pack()
-cb.focus()
 
-if os.name == 'nt':
-    import windows_ops
+def on_text_change(sv):
+    print sv.get()
+    update_select_options(sv.get())
 
-    windows_ops.register_hot_key()
-    root.after(1, windows_ops.hotkey_handler, root)
+
+def on_type_in_entry(event):
+    if event.keysym in {'Up', 'Down'}:
+        print "ignoring", event.keycode, event.keysym
+        listbox.focus_set()
+        listbox.select_set(0)
+        return "break"
+
+
+def on_type_on_list_box(event):
+    print event.keycode, event.keysym
+    if (len(event.keysym) == 1 and event.keysym.isalpha()):
+        entry.focus_set()
+        print "[input]key down: ", event.keysym
+        sv.set(sv.get() + event.keysym)
+    elif (event.keysym == "BackSpace"):
+        entry.focus_set()
+        sv.set(sv.get()[:-1])
+    elif (event.keysym == "Return"):
+        if listbox.get(ACTIVE):
+            go_to(listbox.get(ACTIVE))
+
+
+def go_to(item):
+    print "go to: ", item
+    subprocess.call(['open', 'http://google.com?q=' + item])
+
+
+def register_key():
+    if os.name == 'nt':
+        import windows_ops
+        windows_ops.register_hot_key()
+        root.after(1, windows_ops.hotkey_handler, root)
+    else:
+        print "not supported yet"
+
+
+sv = StringVar()
+sv.trace("w", lambda name, index, mode, sv=sv: on_text_change(sv))
+entry = Entry(root, textvariable=sv)
+entry.bind('<Key>', on_type_in_entry)
+entry.pack()
+
+listbox = Listbox(root)
+listbox.pack()
+listbox.insert(END, "type a keyword")
+listbox.bind('<Key>', on_type_on_list_box)
 
 root.mainloop()
